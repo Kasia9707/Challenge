06eb0a737dabbf88565702d61862be8543b8753b
Kasia Adamska <katada9707@wp.pl>
2023-07-27 14:48:22 +0200

    Zadanie domowe dzień 4

---
+++ Zadanie domowe dzień 4/Zadanie domowe dzień 4/Program.cs
@@ -0,0 +1,16 @@
+﻿string name = "Kasia";
+var age = 26;
+bool woman = true;
+
+if (woman = true & age > 30)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (woman = true & age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (woman = false & age == 26)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
