3591ade27b1ee834df4f02a69254ab49a6259712
Kasia Adamska <katada9707@wp.pl>
2023-07-27 15:55:21 +0200

    Zadanie domowe 4 poprawione

--- Zadanie domowe dzień 4/Zadanie domowe dzień 4/Program.cs
+++ Zadanie domowe dzień 4/Zadanie domowe dzień 4/Program.cs
@@ -2,15 +2,15 @@
 var age = 26;
 bool woman = true;
 
-if (woman = true & age > 30)
+if (woman == true && age == 33)
 {
     Console.WriteLine("Ewa lat 33");
 }
-else if (woman = true & age < 30)
+else if (woman == true && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (woman = false & age == 26)
+else if (woman == false && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
